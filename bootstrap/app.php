0ac58618ffd0ac5518db7a8c90289c6a166a1aa6
FarrukhShomurodov <uamerike@gmail.com>
2024-12-09 17:23:28 +0500

    auth test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(function (\Illuminate\Http\Request $request) {
             $guard = $request->header('guard');
-            dd($request->headers->all());
+            dd($request->route()->getActionMethod());
             if ($guard == 'user') {
                 route('auth.view');
             } else {
