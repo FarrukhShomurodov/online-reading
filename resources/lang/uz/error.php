c905c77efee7bbcb5c6461810875a49b8269bb56
FarrukhShomurodov <uamerike@gmail.com>
2024-12-12 21:01:22 +0500

    errors page added

---
+++ resources/lang/uz/error.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '404' => '404 - Sahifa topilmadi',
+    '404_title' => 'Uzr, izlayotgan sahifangiz topilmadi.',
+    '500' => '500 - Ichki server xatosi',
+    '500_title' => "Uzr, serverda xato yuz berdi. Iltimos, keyinroq qaytib ko'ring",
+];
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,8 @@
+@extends('site.layouts.app')
+
+@section('content')
+    <div class="d-flex w-100 h-100 min-vh-100 flex-column justify-content-center align-items-center">
+        <h1>@lang('error.500')</h1>
+        <p>@lang('error.500_title')</p>
+    </div>
+@endsection
